7ab449cae1b3ced20ce95aff1aff45efbc1b45bd
Krishnendu Ghata <web.way2krish@gmail.com>
2022-01-23 10:51:49 +0000

    changes for my requirement

--- test_code/cartoonize.py
+++ test_code/cartoonize.py
@@ -6,7 +6,27 @@ import network
 import guided_filter
 from tqdm import tqdm
 
+input_photo = None
+final_out = None
+sess = None
 
+def init(model_path): 
+    global input_photo, final_out, sess
+
+    input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
+    network_out = network.unet_generator(input_photo)
+    final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
+
+    all_vars = tf.trainable_variables()
+    gene_vars = [var for var in all_vars if 'generator' in var.name]
+    saver = tf.train.Saver(var_list=gene_vars)
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    sess = tf.Session(config=config)
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, tf.train.latest_checkpoint(model_path))
 
 def resize_crop(image):
     h, w, c = np.shape(image)
@@ -22,39 +42,22 @@ def resize_crop(image):
     return image
     
 
-def cartoonize(load_folder, save_folder, model_path):
-    input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
-    network_out = network.unet_generator(input_photo)
-    final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
+def cartoonize(image):
+    try:
+        image = resize_crop(image)
+        batch_image = image.astype(np.float32)/127.5 - 1
+        batch_image = np.expand_dims(batch_image, axis=0)
+        output = sess.run(final_out, feed_dict={input_photo: batch_image})
+        output = (np.squeeze(output)+1)*127.5
+        output = np.clip(output, 0, 255).astype(np.uint8)
+        
+        return output
+    except:
+        print('cartoonize {} failed'.format(load_path))
 
-    all_vars = tf.trainable_variables()
-    gene_vars = [var for var in all_vars if 'generator' in var.name]
-    saver = tf.train.Saver(var_list=gene_vars)
-    
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-
-    sess.run(tf.global_variables_initializer())
-    saver.restore(sess, tf.train.latest_checkpoint(model_path))
-    name_list = os.listdir(load_folder)
-    for name in tqdm(name_list):
-        try:
-            load_path = os.path.join(load_folder, name)
-            save_path = os.path.join(save_folder, name)
-            image = cv2.imread(load_path)
-            image = resize_crop(image)
-            batch_image = image.astype(np.float32)/127.5 - 1
-            batch_image = np.expand_dims(batch_image, axis=0)
-            output = sess.run(final_out, feed_dict={input_photo: batch_image})
-            output = (np.squeeze(output)+1)*127.5
-            output = np.clip(output, 0, 255).astype(np.uint8)
-            cv2.imwrite(save_path, output)
-        except:
-            print('cartoonize {} failed'.format(load_path))
-
-
-    
+def call_cartoonize(ip_file, op_file):
+    image = cv2.imread(ip_file)
+    cv2.imwrite(op_file, cartoonize(image))
 
 if __name__ == '__main__':
     model_path = 'saved_models'
@@ -62,7 +65,20 @@ if __name__ == '__main__':
     save_folder = 'cartoonized_images'
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
-    cartoonize(load_folder, save_folder, model_path)
+
+    init(model_path)
+
+    ip_file = os.path.join(load_folder, 'actress2.jpg')
+    op_file = os.path.join(save_folder, 'actress2.jpg')
+    call_cartoonize(ip_file, op_file)
+    
+    ip_file = os.path.join(load_folder, 'party5.jpg')
+    op_file = os.path.join(save_folder, 'party5.jpg')
+    call_cartoonize(ip_file, op_file)
+
+    ip_file = os.path.join(load_folder, 'party7.jpg')
+    op_file = os.path.join(save_folder, 'party7.jpg')
+    call_cartoonize(ip_file, op_file)
     
 
     
